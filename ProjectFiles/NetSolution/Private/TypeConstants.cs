7083b7dd59b1652af719ace853d4698d64a7de93
tliu2 <tliu2@ra.rockwell.com>
2024-01-30 10:12:05 +0800

    initial

---
+++ ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,44 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace ProjectCreatedWithVersion_1_4_LL
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("ProjectCreatedWithVersion_1_4_LL");
+        public static readonly NodeId EditUserDetailPanel = new NodeId(namespaceIndex, new Guid("5bd1738d3f60d47ba30b9df411fa0050"));
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("bb3ffd2e45ccdf85b7f023208754721d"));
+        public static readonly NodeId ChangeUserDialog = new NodeId(namespaceIndex, new Guid("4c468082fbaefdd3984909eff45946cc"));
+        public static readonly NodeId AnalogAlarmGrid = new NodeId(namespaceIndex, new Guid("37442aa2bd8928c465725a9d655a4f8f"));
+        public static readonly NodeId CreateUserPanel = new NodeId(namespaceIndex, new Guid("76c6b9dc24666b9297ce397d3fb2b9fe"));
+        public static readonly NodeId AlarmGrid = new NodeId(namespaceIndex, new Guid("71cef6cb9448ed8eec272a74a2aac225"));
+        public static readonly NodeId RecipesEditor = new NodeId(namespaceIndex, new Guid("d8c1f937b06701ef258bf0ca9bebb5a9"));
+        public static readonly NodeId User_Editor_Panel_中é = new NodeId(namespaceIndex, new Guid("a595e6d9267daf5b97d9114159b29575"));
+        public static readonly NodeId Alarm_Grid_Panel_中é = new NodeId(namespaceIndex, new Guid("469b7710941a259b265c5f8e91994f67"));
+        public static readonly NodeId Recipes_Editor_Panel_中é = new NodeId(namespaceIndex, new Guid("1b61db0c3d363c14fced133a95fcac86"));
+        public static readonly NodeId LoginDialog_中é = new NodeId(namespaceIndex, new Guid("bcb75da440fe4d6a88402c472e471d51"));
+        public static readonly NodeId LoginForm = new NodeId(namespaceIndex, new Guid("d0fba00ff93f064d6e1cb546eea27209"));
+        public static readonly NodeId UserEditorOverview = new NodeId(namespaceIndex, new Guid("fd52139656c4cab63738095f771343a3"));
+        public static readonly NodeId NoUsersPanel = new NodeId(namespaceIndex, new Guid("638838acd9f5ae93ca75b058c300b43b"));
+        public static readonly NodeId GroupsPanel = new NodeId(namespaceIndex, new Guid("d312f5365c7d886a7a689a7c61bf2412"));
+        public static readonly NodeId GroupCheckbox = new NodeId(namespaceIndex, new Guid("937bdaeca748212659105fb563595c0b"));
+        public static readonly NodeId GroupLabel = new NodeId(namespaceIndex, new Guid("59f233a86b88133bc07cecf82da51463"));
+        public static readonly NodeId LocaleComboBox = new NodeId(namespaceIndex, new Guid("b6239b77b31bba48a937ee481e521c37"));
+        public static readonly NodeId ChangeUserForm = new NodeId(namespaceIndex, new Guid("aafae88a96cb862a3d9a244021075150"));
+        public static readonly NodeId ChangeUserPasswordExpiredDialog = new NodeId(namespaceIndex, new Guid("fdec4df0661b496689dfbf9cfcb33ae1"));
+        public static readonly NodeId ChangeUserChangePasswordForm = new NodeId(namespaceIndex, new Guid("e98f071e78271b043e525124cfadd8f9"));
+        public static readonly NodeId Login = new NodeId(namespaceIndex, new Guid("ce814fddddd607c776a3b9a6e0b90c27"));
+        public static readonly NodeId LoginPasswordExpiredDialog = new NodeId(namespaceIndex, new Guid("53b1bd1b17b4de66926108a7dfdf143e"));
+        public static readonly NodeId Logout = new NodeId(namespaceIndex, new Guid("2eebfd3def044e747aa57952fc13c70d"));
+        public static readonly NodeId LoginChangePasswordForm = new NodeId(namespaceIndex, new Guid("fc0d46c3cf6fc6f7faace8fa13bde2b5"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("ProjectCreatedWithVersion_1_4_LL");
+    }
+}
---
+++ ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,131 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "5bd1738d3f60d47ba30b9df411fa0050")]
+public class EditUserDetailPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "bb3ffd2e45ccdf85b7f023208754721d")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "4c468082fbaefdd3984909eff45946cc")]
+public class ChangeUserDialog : FTOptix.UI.Dialog
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "37442aa2bd8928c465725a9d655a4f8f")]
+public class AnalogAlarmGrid : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "76c6b9dc24666b9297ce397d3fb2b9fe")]
+public class CreateUserPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "71cef6cb9448ed8eec272a74a2aac225")]
+public class AlarmGrid : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "d8c1f937b06701ef258bf0ca9bebb5a9")]
+public class RecipesEditor : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "a595e6d9267daf5b97d9114159b29575")]
+public class User_Editor_Panel_中é : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "469b7710941a259b265c5f8e91994f67")]
+public class Alarm_Grid_Panel_中é : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "1b61db0c3d363c14fced133a95fcac86")]
+public class Recipes_Editor_Panel_中é : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "bcb75da440fe4d6a88402c472e471d51")]
+public class LoginDialog_中é : FTOptix.UI.Dialog
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "d0fba00ff93f064d6e1cb546eea27209")]
+public class LoginForm : FTOptix.UI.PanelLoader
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "fd52139656c4cab63738095f771343a3")]
+public class UserEditorOverview : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "638838acd9f5ae93ca75b058c300b43b")]
+public class NoUsersPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "d312f5365c7d886a7a689a7c61bf2412")]
+public class GroupsPanel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "937bdaeca748212659105fb563595c0b")]
+public class GroupCheckbox : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "59f233a86b88133bc07cecf82da51463")]
+public class GroupLabel : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "b6239b77b31bba48a937ee481e521c37")]
+public class LocaleComboBox : FTOptix.UI.ComboBox
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "aafae88a96cb862a3d9a244021075150")]
+public class ChangeUserForm : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "fdec4df0661b496689dfbf9cfcb33ae1")]
+public class ChangeUserPasswordExpiredDialog : FTOptix.UI.Dialog
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "e98f071e78271b043e525124cfadd8f9")]
+public class ChangeUserChangePasswordForm : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "ce814fddddd607c776a3b9a6e0b90c27")]
+public class Login : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "53b1bd1b17b4de66926108a7dfdf143e")]
+public class LoginPasswordExpiredDialog : FTOptix.UI.Dialog
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "2eebfd3def044e747aa57952fc13c70d")]
+public class Logout : FTOptix.UI.Panel
+{
+}
+
+[MapType(NamespaceUri = "ProjectCreatedWithVersion_1_4_LL", Guid = "fc0d46c3cf6fc6f7faace8fa13bde2b5")]
+public class LoginChangePasswordForm : FTOptix.UI.Panel
+{
+}
